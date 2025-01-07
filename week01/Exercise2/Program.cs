7a9b6ded189c319aa3d6ced08137c6febaad74d3
s1mply1958 <pdbest1958@yahoo.ca>
2025-01-07 13:28:33 -0700

    Code written

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,81 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percent? ");
+        string percent = Console.ReadLine();
+
+        int grade = int.Parse(percent);
+
+        string letter;
+        
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "D";
+        }
+
+        // Stretch Challenge 1
+        string sign = "";
+
+        int lastDigit = grade % 10;
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+
+        else
+        {
+            sign = "";
+        }
+
+        // Stretch Challenge 2
+        if (grade >= 93)
+        {
+            sign = "";
+        }
+
+        // Stretch Challenge 3
+        
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.Write($"Your letter grade is: {letter}{sign} ");
+
+        if (grade >= 70)
+        {
+            Console.Write("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.Write("Stay focused and you'll get it next time!");
+        }
     }
 }
