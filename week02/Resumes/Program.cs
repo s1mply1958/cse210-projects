70ae834368e74900d2a6ce9face294189530a37e
s1mply1958 <pdbest1958@yahoo.ca>
2025-01-10 11:44:23 -0700

    In progress

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        //Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "(Microsoft)";
+        job1._startYear = 2021;
+        job1._endYear = 2024;
     }
+    
 }
