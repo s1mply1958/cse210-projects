f118d324bf93f77d3f262e1e3b1776cc347362f9
s1mply1958 <pdbest1958@yahoo.ca>
2025-01-10 14:25:07 -0700

    Step 6 work in progress

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,12 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Console.WriteLine("Hello World! This is the Resumes Project.");
+        Resume Resume1 = new Resume();
+        Resume1._name = "Paul Best";
+        Resume1._jobs = "Junior Developer";
+
+        Resume1.Display();
+
+
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "(Microsoft)";
-        job1._startYear = 2021;
-        job1._endYear = 2024;
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        job1.Display();
+    
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "(Apple)";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        job2.Display();
+
     }
     
 }
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Resume
+{
+    public string _name;
+    public string _jobs;
+    public void Display()
+
+    {
+        Console.WriteLine($"{_name} {_jobs}");
+     
+    }
+
+}
