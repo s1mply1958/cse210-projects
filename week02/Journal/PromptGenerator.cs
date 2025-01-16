77d49a44a5a11c9b22e70832e6095044e447671c
s1mply1958 <pdbest1958@yahoo.ca>
2025-01-16 14:19:31 -0700

    Prompt questions created & random selection initiated

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,10 +1,27 @@
+using System;
+using System.Collections.Generic;
+
 public class PromptGenerator
 {
-    public List<string> _prompts;
-      
+    public List<string> _prompts = new List<string>();
+
+    public void Prompts()
+    {
+        _prompts.Add("How did you see the hand of the Lord in your life today?");
+        _prompts.Add("How did you hear Him today?");
+        _prompts.Add("What did you accomplish today?");
+        _prompts.Add("Describe three blessings recieved today?");
+        _prompts.Add("Describe something that you learned today?");        
+    }
+
+
+
     public string GetRandomPrompt()
     {
-        return "";
+        Random inspiration = new Random();
+        int question = inspiration.Next(_prompts.Count);
+
+        return _prompts[question];
     }
 
 }
