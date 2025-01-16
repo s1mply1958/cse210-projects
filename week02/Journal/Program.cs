b192df442b84016c1788bdd4c984cee786660797
s1mply1958 <pdbest1958@yahoo.ca>
2025-01-16 10:21:39 -0700

    Class file stubs & program menu defined

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+    //Console.WriteLine("Hello World! This is the Journal Project.");
+    Console.WriteLine("Welcome to the Journal Program!");
+    Console.WriteLine("Please select one of the following choices:");
+    Console.WriteLine("1. Write");
+    Console.WriteLine("2. Display");
+    Console.WriteLine("3. Load");
+    Console.WriteLine("4. Save");
+    Console.WriteLine("5. Quit");
+    Console.Write("What would you like to do? ");
+
+    string entry = Console.ReadLine();
+    int choice = int.Parse(entry);
+
+    // PromptGenerator 
+
+
+    // Journal myJournal = new Journal();
+
+
+
+    //Entry myEntry = new Entry();
+
+    //myEntry.Display();
     }
 }
