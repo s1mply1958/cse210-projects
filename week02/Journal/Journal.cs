14d7ed72034c141bf843b54cb58e4dd4fbf5bd2a
s1mply1958 <pdbest1958@yahoo.ca>
2025-01-17 10:29:34 -0700

    DisplayAll, SaveToFile added

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 public class Journal
 {
@@ -11,11 +12,20 @@ public class Journal
     }
     public void DisplayAll()
     {
-
+        foreach(Entry entry in _entries)
+        {
+            entry.Display();
+        }
     }
-    public void SaveToFile(string file)
+    public void SaveToFile(string filename)
     {
-    
+        using(StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach(Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date}\n{entry._promptText}\n{entry._entryText}");
+            }
+        }
     }
     public void LoadFromFile(string file)
     {
