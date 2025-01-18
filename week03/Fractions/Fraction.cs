d9c8ea1f34e806df2a393d6a2396f7a0e60e740d
s1mply1958 <pdbest1958@yahoo.ca>
2025-01-18 11:12:43 -0700

    Class file name changed to Fraction from Fractions, initial parameters defined, Program file has errors.

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,82 @@
+using System;
+
+public class Fraction
+{
+
+    private int _top;
+    private int _bottom;
+
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+        
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+
+
+    }
+
+
+    // public void GetTop()
+    // {
+
+    // }
+    // public void SetTop(int top)
+    // {
+
+    // }
+    // public void GetBottom()
+    // {
+
+    // }
+    // public void SetBottom(int bottom)
+    // {
+
+    // }
+
+
+    // public string GetFractionString()
+    // {
+
+    // }
+    // public double GetDecimalValue()
+    // {
+
+    // }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        // Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction();
+        {
+            Console.WriteLine(fraction1._top);
+            Console.WriteLine(fraction1._bottom);
+        
+        }
     }
 }
