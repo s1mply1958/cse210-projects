4a5ec35b6b179232136c29afca680ef9552a42f2
s1mply1958 <pdbest1958@yahoo.ca>
2025-01-18 10:22:53 -0700

    Fraction class with errors

---
+++ week03/Fractions/Fractions.cs
@@ -0,0 +1,76 @@
+using System;
+
+public class Fractions
+{
+
+    private int _top;
+    private int _bottom;
+
+
+    public void Fraction()
+    {
+
+    }
+    public void Fraction(int wholeNumber)
+    {
+
+    }
+    public void Fraction(int top,int bottom)
+    {
+
+    }
+
+
+    public void GetTop()
+    {
+
+    }
+    public void SetTop(int top)
+    {
+
+    }
+    public void GetBottom()
+    {
+
+    }
+    public void SetBottom(int bottom)
+    {
+
+    }
+
+
+    public string FractionString()
+    {
+
+    }
+    public double GetDecimal()
+    {
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
