d68178348b35eaba6eea5c0ee36e576d089b207e
s1mply1958 <pdbest1958@yahoo.ca>
2025-01-23 16:10:33 -0700

    Member variable behaviors defined

--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -7,29 +7,34 @@ private bool _isHidden;
 
 
     public Word(string text)
-{
-    
-}
+    {
+        _text = text;
+        _isHidden = false;
+
+    }
 
     public void Hide()
-{
+    {
+        _isHidden = true;
 
-}
+    }
 
-   public void Show()
-{
+    public void Show()
+    {
+        _isHidden = false;
 
-} 
+    } 
 
-public bool IsHidden()
-{
-    return true;
-}
+    public bool IsHidden()
+    {
+        return _isHidden;
+        
+    }
 
     public string GetDisplayText()
-{
-    return "";
-}
+    {
+        return "";
+    }
 
 
 }
