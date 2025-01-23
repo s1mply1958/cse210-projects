caabc509dedae86078f438b0ca4924018bbfd957
s1mply1958 <pdbest1958@yahoo.ca>
2025-01-22 21:15:05 -0700

    Class file stubs created

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference Reference, string text)
+    {
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        
+    }
+    
+    public string GetDisplayText()
+    {
+    return "";
+    }
+
+    public bool IsCompletelyHidden()
+{
+    return  true; 
+}
+
+
+}
---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,35 @@
+using System;
+
+public class Word
+{
+private string _text;
+private bool _isHidden;
+
+
+    public Word(string text)
+{
+    
+}
+
+    public void Hide()
+{
+
+}
+
+   public void Show()
+{
+
+} 
+
+public bool IsHidden()
+{
+    return true;
+}
+
+    public string GetDisplayText()
+{
+    return "";
+}
+
+
+}
