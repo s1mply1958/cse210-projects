2fd388ab06695b906531c6de2658aa860e669a02
s1mply1958 <pdbest1958@yahoo.ca>
2025-01-23 21:54:23 -0700

    Build out Scripture Reference & HideRandomWords constructors.

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,29 +1,47 @@
 using System;
+using System.Collections.Generic;
 
 public class Scripture
 {
     private Reference _reference;
     private List<Word> _words;
 
-    public Scripture(Reference Reference, string text)
+    public Scripture(Reference reference, string text)
     {
+        _reference = reference;
+        _words = new List<Word>();
+
+        foreach (var word in text.Split(' '))
+        {
+            _words.Add(new Word(word));
+            
+        }
 
     }
 
     public void HideRandomWords(int numberToHide)
     {
-        
+        Random random = new Random();
+
+        for (int i = 0; i < numberToHide; i++)
+        {
+            int index = random.Next(_words.Count);
+
+            _words[index].Hide();
+
+        }
+
     }
     
     public string GetDisplayText()
     {
-    return "";
+        return "";
     }
 
     public bool IsCompletelyHidden()
-{
-    return  true; 
-}
+    {
+        return  true; 
+    }
 
 
 }
